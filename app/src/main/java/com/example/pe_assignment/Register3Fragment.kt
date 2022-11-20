02784a634dbbaffc25d677f1e8f5cc7f8dec53bf
zhixuanjj <108249653+lzxjj@users.noreply.github.com>
2022-11-20 20:52:26 +0800

    11.20
    register done

--- app/src/main/java/com/example/pe_assignment/Register3Fragment.kt
+++ app/src/main/java/com/example/pe_assignment/Register3Fragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.navigation.findNavController
 
 class Register3Fragment : Fragment() {
         override fun onCreateView(
@@ -15,5 +17,35 @@ class Register3Fragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register_3, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // access the items of the list
+        val languages = resources.getStringArray(R.array.Languages)
 
-}
+        // access the spinner
+        val spinner = getView()?.findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = activity?.let {
+                ArrayAdapter(
+                    it,
+                    android.R.layout.simple_spinner_item, languages)
+            }
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+//                    Toast.makeText(this@Register3Fragment,
+//                        getString(R.string.selected_item) + " " +
+//                                "" + languages[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+
+    }
+
+}}
