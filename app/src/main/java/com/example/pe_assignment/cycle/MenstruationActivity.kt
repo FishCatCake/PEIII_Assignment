379626a98805011c7f36e18dae1a91730e575147
LYc <SWE1909484@xmu.edu.my>
2022-11-29 22:04:04 +0800

    menstruation page done

---
+++ app/src/main/java/com/example/pe_assignment/cycle/MenstruationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pe_assignment.cycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pe_assignment.R
+
+class MenstruationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_cycle)
+    }
+}
