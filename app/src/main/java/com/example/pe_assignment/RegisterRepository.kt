f717eff3af7064350ec28f9ae0c1a9e15788e3b4
shenyuxuan <shenyuxuan8@users.noreply.github.com>
2022-12-18 15:40:49 +0800

    Part of login database

---
+++ app/src/main/java/com/example/pe_assignment/RegisterRepository.kt
@@ -0,0 +1,21 @@
+package com.example.pe_assignment
+
+import kotlinx.coroutines.flow.Flow
+
+class RegisterRepository (private val dao: UserDao){
+    // Register / Sign Up
+    suspend fun insert(user: User) {
+        dao.insert(user)
+    }
+
+    // Get user list
+    var userList: Flow<List<User>> = dao.getUsers()
+    fun getUsers(): Flow<List<User>> {
+        return userList
+    }
+
+    // Login
+    suspend fun getUserCredential(userName: String): User {
+        return dao.getUserCredential(userName)
+    }
+}
