d4270b81f8e8e15f6d4e3e3a9ff0033bae2c5052
zhixuanjj <108249653+lzxjj@users.noreply.github.com>
2022-12-24 13:02:03 +0800

    Revert "Revert "Merge branch 'yuxuan' into LYC""
    This reverts commit 78377d2be829220fbb51e4968dc1135aeb1ac552.

--- app/src/main/java/com/example/pe_assignment/login/LoginActivity.kt
+++ app/src/main/java/com/example/pe_assignment/login/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.example.pe_assignment.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.pe_assignment.R
+import com.example.pe_assignment.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        @Suppress("UNUSED VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityLoginBinding>(this,R.layout.activity_login)
     }
 }
