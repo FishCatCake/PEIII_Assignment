bf8b70b31740708cc0c75e363553fec7bd42a1db
zhixuanjj <108249653+lzxjj@users.noreply.github.com>
2022-12-28 09:50:44 +0800

    12.28
    Delete useless files

--- app/src/main/java/com/example/pe_assignment/hpv/DoseCalendarFragment.kt
+++ app/src/main/java/com/example/pe_assignment/hpv/DoseCalendarFragment.kt
@@ -15,14 +15,6 @@ import com.example.pe_assignment.R
 
 class DoseCalendarFragment : Fragment() {
 
-    private lateinit var adapter: HpvCalendarAdapter
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var calendarArrayList: ArrayList<HpvCalendarModel>
-    lateinit var page : Array<String>
-    lateinit var dose : Array<String>
-//    lateinit var calendar : Array<Int>
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,33 +38,5 @@ class DoseCalendarFragment : Fragment() {
             view.findNavController().navigate(R.id.reviewCalendarFragment)
         }
 
-    }
-
-    private fun dataInitialize(){
-        calendarArrayList = arrayListOf<HpvCalendarModel>()
-        page = arrayOf(
-            "1/3",
-            "2/3",
-            "3/3"
-        )
-
-        dose = arrayOf(
-            "Date of First " +
-                    "Dose Onset",
-            "Date of Second " +
-                    "Dose Onset",
-            "Date of Third " +
-                    "Dose Onset"
-        )
-
-//        calendar = arrayOf(
-//            1,
-//            2,
-//            3
-//        )
-        for(i in page.indices){
-            val calendar = HpvCalendarModel(page[i],dose[i])
-            calendarArrayList.add(calendar)
-        }
     }
 }
